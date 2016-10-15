f029974ceefb098124aed2e839ecbacb7dee40b5
jeff3456 <jeff3456@gmail.com>
2016-10-15 02:12:06 -0400

    testing processing animation

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -3,6 +3,179 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+
+var mBoxGuy;
+var mBalls;
+var mCanvasWidth;
+var mCanvasHeight;
+
+/* http://www.color-hex.com/color-palette/24687 */
+var COLORS_RGB = {
+	YELLOW: [240,204,89],
+	ORANGE: [229,98,59],
+	RED: [201,41,53],
+	GREEN: [91,212,91],
+	BLUE: [6,162,178]
+}
+
+function hitWall(object){
+	if(object.x < 0) return true;
+	if(object.y < 0) return true;
+	if(object.x > mCanvasWidth) return true;
+	if(object.y > mCanvasHeight) return true;
+	return false;
+}
+
+/*
+	DEF for Ball object
+*/
+
+function Ball(x, y, size, gConstant, orbitSpeed, maxSpeed, radiusZero, color){
+	this.gConstant = gConstant;
+	this.x = x;
+	this.y = y;
+	this.xV = 0;
+	this.yV = 0;
+	this.size = size;
+	this.orbitSpeed = orbitSpeed;
+	this.maxSpeed = maxSpeed;
+	this.radiusZero = radiusZero;
+	this.color = COLORS_RGB[color];
+}
+
+Ball.prototype.move = function(destX, destY){
+	// calculate acceleration to box;
+	var d = distance(this.x, this.y, destX, destY);
+
+	var accel = 0;
+	if(d > 0 && d > this.radiusZero) accel = this.gConstant/Math.pow(d, 2);
+	var scale = 0;
+	if(d > 0) scale = accel/ d;
+
+
+	// Calculate accel in x and y directions.
+	var xA = (destX - this.x) * scale;
+	var yA = (destY - this.y) * scale;
+
+	// if too close, set accel to 0.
+
+	// based on accel in x and y update velocity
+	this.xV += xA;
+	// Do not go above maxSpeed;
+	if(this.xV < 0 && Math.abs(this.xV) > this.maxSpeed)
+		this.xV = (-1)*this.maxSpeed;
+	else if(this.xV > this.maxSpeed) {
+		this.xV = this.maxSpeed;
+	}
+	this.yV += yA;
+	if(this.yV < 0 && Math.abs(this.yV) > this.maxSpeed)
+		this.yV = (-1)*this.maxSpeed;
+	else if(this.yV > this.maxSpeed) {
+		this.yV = this.maxSpeed;
+	}
+
+	// based on velocity update
+	this.x += this.xV;
+	this.y += this.yV;
+
+	// Change direction if you hit a wall.
+	if(hitWall(this)){
+		if(this.x < 0 && this.xV < 0) this.xV *= -1;
+		if(this.x > mCanvasWidth && this.xV > 0) this.xV *= -1;
+		if(this.y < 0 && this.yV < 0) this.yV *= -1;
+		if(this.y > mCanvasHeight && this.yV > 0) this.yV *= -1;
+	}
+}
+
+
+/*
+	DEF for BoxGuy object
+*/
+function BoxGuy(x, y, accel, size, minSpeed, color){
+	this.x = x;
+	this.y = y;
+	this.accel = accel;
+	this.size = size;
+	this.minSpeed = minSpeed;
+	this.color = COLORS_RGB[color];
+}
+
+BoxGuy.prototype.move = function(destX, destY){
+	var d = distance(this.x, this.y, destX, destY);
+	var speed = 0;
+	if(d > 2) speed = Math.max(this.accel * Math.pow(d, 2), this.minSpeed);
+	var scale = 0;
+	if(d !== 0) scale = speed/ d;
+	var dX = (destX - this.x) * scale;
+	var dY = (destY - this.y) * scale;
+
+	this.x += dX;
+	this.y += dY;
+}
+
+function distance(x1, y1, x2, y2){
+	var d = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+	return d;
+}
+
+function drawBoxGuy(bxg){
+	// draw square around boxGuy
+	var offset = bxg.size/2;
+	var c = color(bxg.color[0], bxg.color[1], bxg.color[2]);
+	fill(c);  // Use color variable 'c' as fill color
+
+	rect(bxg.x-offset, bxg.y-offset, bxg.size, bxg.size, 10);
+	// Draw eyes and mouth
+	// ellipse();
+}
+function drawBall(ball){
+	var c = color(ball.color[0], ball.color[1], ball.color[2]);
+	fill(c);  // Use color variable 'c' as fill color
+
+	ellipse(ball.x, ball.y, ball.size);
+}
+
+
+
+function setup() {
+	mCanvasHeight = $(window).height();
+	mCanvasWidth = $(window).width();
+  createCanvas(mCanvasWidth, mCanvasHeight);
+	noStroke();  // Don't draw a stroke around shapes
+
+	// BoxGuy(x, y, accel, size, minSpeed, color)
+	mBoxGuy = new BoxGuy(mCanvasWidth/2, mCanvasHeight/2, 0.0004, 80, 2, "RED");
+
+	// init balls
+	mBalls = [];
+	// Ball(x, y, size, gConstant, orbitSpeed, maxSpeed, radiusZero)
+	mBalls.push(new Ball(mBoxGuy.x-50, mBoxGuy.y+50, 20, 7000, 5, 15, 60, "GREEN"));
+	mBalls.push(new Ball(mBoxGuy.x+50, mBoxGuy.y-50, 25, 4000, 5, 12, 60, "YELLOW"));
+	mBalls.push(new Ball(mBoxGuy.x, mBoxGuy.y+80, 35, 3000, 5, 6, 50, "BLUE"));
+}
+
+
+function draw() {
+	clear();
+	mBoxGuy.move(mouseX, mouseY);
+	for(var i = 0; i < mBalls.length; i++){
+		mBalls[i].move(mBoxGuy.x, mBoxGuy.y);
+		drawBall(mBalls[i]);
+	}
+	drawBoxGuy(mBoxGuy);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function incrPixel(str) {
 	return (parseInt(str.replace("px", "")) - 10) + "px";
 }
@@ -94,8 +267,7 @@ var spriteWidth = parseInt($(".sprite").css("width").replace("px", ""));
 				$("#pacman").css("left", decrPixel($("#pacman").css("left")))
 				break;
 		}
-		}
-	);
+	});
 	/*window.setInterval(function() {
 		var posX = parseInt($("#pacman").css("left").replace("px", ""));
 		if (posX < 0 || posX > width - spriteWidth) {
